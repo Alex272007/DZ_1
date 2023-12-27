7da6fedc9b530c506d4263e0df13d9669476dc53
Aleksey Kvashnin <aleks272007@gmail.com>
2023-12-27 18:07:32 +0400

    add dz_1:task_1

---
+++ dz_1/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// Проверяем какое число кратное одновременно и 7 и 23(7 * 23 = 161)
+if (num % 161 == 0)
+{
+    Console.WriteLine($"Число {num} одновременно кратно 7 и 23"); 
+}
+else
+{
+    Console.WriteLine($"Число {num} одновременно не кратно 7 и 23");
+}
