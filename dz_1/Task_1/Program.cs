8c516ee66b6e25b9aadc7bee21d3720e7a03cf6f
Aleksey Kvashnin <aleks272007@gmail.com>
2023-12-28 09:28:45 +0400

    add dz_1:task_1

--- dz_1/Task_1/Program.cs
+++ dz_1/Task_1/Program.cs
@@ -1,7 +1,8 @@
 ﻿Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+int a = 7 * 23;
 // Проверяем какое число кратное одновременно и 7 и 23(7 * 23 = 161)
-if (num % 161 == 0)
+if (num % a == 0)
 {
     Console.WriteLine($"Число {num} одновременно кратно 7 и 23"); 
 }
--- dz_1/Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
+++ dz_1/Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6fcc58b339a2874dde192739f06c9c93c3b9c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
