70203066333fb74c6a9e82b9dcb8a8aedaa00d3a
Aleksey Kvashnin <aleks272007@gmail.com>
2023-12-29 18:41:30 +0400

    add dz_1:task_2

---
+++ dz_1/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите координаты x: ");
+int x_coordinates = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты y: ");
+int y_coordinates = Convert.ToInt32(Console.ReadLine());
+
+if (x_coordinates > 0 && y_coordinates > 0)
+{
+    Console.WriteLine("Эта точка в 1 координатной четверти плоскости!");
+}
+else if (x_coordinates < 0 && y_coordinates > 0)
+{
+    Console.WriteLine("Эта точка во 2 координатной четверти плоскости!");
+}
+else if (x_coordinates < 0 && y_coordinates < 0)
+{
+    Console.WriteLine("Эта точка в 3 координатной четверти плоскости!");
+}
+else 
+{
+    Console.WriteLine("Эта точка в 4 координатной четверти плоскости!");
+}
