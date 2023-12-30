eb355eaaf109630089bcc105aa7a932b36732097
Aleksey Kvashnin <aleks272007@gmail.com>
2023-12-30 16:58:00 +0400

    add dz_1:task_4

---
+++ dz_1/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int divider = 1;
+int digit = 0;
+
+ while (number / divider >= 10)
+ {
+    divider = divider * 10;
+ }
+ while (divider > 0)
+ {
+    digit = number / divider;
+    number = number % divider;
+    divider = divider / 10;
+    Console.Write(digit + ", ");
+ }
