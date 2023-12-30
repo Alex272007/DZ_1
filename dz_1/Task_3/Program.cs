604af5c1ad68e6e17d80a0b7df41d076737b4dd0
Aleksey Kvashnin <aleks272007@gmail.com>
2023-12-30 13:58:09 +0400

    add dz_1:task_3

---
+++ dz_1/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число  целое число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+     
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit + " Наибольшая цифра числа.");
+}
+else if (firstDigit < secondDigit) 
+{
+    Console.WriteLine(secondDigit + " Наибольшая цифра числа.");  
+}
+else
+{
+    Console.WriteLine("Цифры равные");
+}
